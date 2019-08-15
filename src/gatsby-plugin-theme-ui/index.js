70d57469b449458dea7f960de2daa94814afaa70
brenopolanski <breno.polanski@gmail.com>
2019-08-15 16:06:32 -0300

    Update global layout

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -26,13 +26,13 @@ export default {
   ],
   fontWeights: {
     normal: 400,
-    bold: 600,
+    bold: 600
   },
   lineHeights: {
     none: 1,
     tight: 1.25,
     normal: 1.5,
-    loose: 2,
+    loose: 2
   },
   colors: {
     text: '#f0f0f0',
@@ -48,14 +48,14 @@ export default {
       fontFamily: 'sansSerif',
       fontSize: 3,
       lineHeight: 'normal',
-      fontWeight: 'normal',
+      fontWeight: 'normal'
     },
     h1: {
       mt: 6,
       mb: 4,
       fontSize: 6,
       fontWeight: 'bold',
-      lineHeight: 'tight',
+      lineHeight: 'tight'
     },
     h2: {
       mt: 9,
@@ -67,29 +67,29 @@ export default {
       textTransform: 'uppercase',
       letterSpacing: 1,
       borderBottom: '1px solid',
-      borderColor: 'border',
+      borderColor: 'border'
     },
     h3: {
       mt: 7,
       mb: 2,
       fontSize: 5,
       lineHeight: 'tight',
-      fontWeight: 'bold',
+      fontWeight: 'bold'
     },
     p: {
       mt: 0,
-      mb: 4,
+      mb: 4
     },
     ul: {
       p: 0,
-      listStyle: 'none',
+      listStyle: 'none'
     },
     li: {
       mt: 0,
       mb: 2,
       '&::before': {
         content: '"•"',
-        pr: 4,
+        pr: 4
       }
     },
     a: {
@@ -108,7 +108,7 @@ export default {
       my: 9,
       border: 0,
       height: 1,
-      backgroundColor: 'border',
+      backgroundColor: 'border'
     }
   }
 }
