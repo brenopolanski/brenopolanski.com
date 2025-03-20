a0bc0a0fb1ff585c5955657d21d32b26ff0678d1
brenopolanski <breno.polanski@gmail.com>
2025-03-20 09:30:56 -0300

    refactor: remove ignore for css and shadcn-ui files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,6 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
-    ignores: ['src/styles/**', 'src/components/ui/**'],
     plugins: {
       'simple-import-sort': simpleImportSort,
       'sort-keys-custom-order-fix': sortKeysCustomOrderFix,
