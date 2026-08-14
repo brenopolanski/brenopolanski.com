0910c787431ff190ac7ffa07806b741f51677c8b
brenopolanski <breno.polanski@gmail.com>
2026-08-14 17:02:41 -0300

    chore: remove next lint and use eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,5 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { FlatCompat } from '@eslint/eslintrc'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import unusedImports from 'eslint-plugin-unused-imports'
@@ -11,77 +13,73 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [
-  {
-    ignores: ['.next/**', '.source/**', 'out/**', 'build/**', 'next-env.d.ts', '**/src/components/ui/**'],
+const eslintConfig = [{
+  ignores: ['.next/**', '.source/**', 'out/**', 'build/**', 'next-env.d.ts', '**/src/components/ui/**'],
+}, ...nextCoreWebVitals, ...nextTypescript, ...compat.extends("prettier"), {
+  plugins: {
+    'simple-import-sort': simpleImportSort,
+    'unused-imports': unusedImports,
   },
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
-  {
-    plugins: {
-      'simple-import-sort': simpleImportSort,
-      'unused-imports': unusedImports,
-    },
-    rules: {
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
-      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/naming-convention': [
-        'error',
-        {
-          format: ['PascalCase'],
-          prefix: ['I'],
-          selector: 'interface',
-        },
-        {
-          format: ['PascalCase'],
-          prefix: ['T'],
-          selector: 'typeAlias',
-        },
-      ],
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-import-type-side-effects': ['error'],
-      '@typescript-eslint/no-non-null-assertion': 'off',
-      curly: 'error',
-      'no-unused-vars': 'off',
-      'object-shorthand': ['error', 'always'],
-      'react/function-component-definition': [
-        'error',
-        {
-          namedComponents: 'arrow-function',
-          unnamedComponents: 'arrow-function',
-        },
-      ],
-      'react/jsx-curly-brace-presence': 'error',
-      'react/jsx-sort-props': [
-        'error',
-        {
-          callbacksLast: true,
-          ignoreCase: true,
-          noSortAlphabetically: false,
-          reservedFirst: true,
-          shorthandLast: true,
-        },
-      ],
-      'react/no-unknown-property': ['error', { ignore: ['global', 'jsx'] }],
-      'react/prop-types': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react-hooks/exhaustive-deps': 'warn',
-      'react-hooks/rules-of-hooks': 'error',
-      'simple-import-sort/exports': 'error',
-      'simple-import-sort/imports': 'error',
-      'unused-imports/no-unused-imports': 'error',
-      'unused-imports/no-unused-vars': [
-        'warn',
-        {
-          args: 'after-used',
-          argsIgnorePattern: '^_',
-          vars: 'all',
-          varsIgnorePattern: '^_',
-        },
-      ],
-    },
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        format: ['PascalCase'],
+        prefix: ['I'],
+        selector: 'interface',
+      },
+      {
+        format: ['PascalCase'],
+        prefix: ['T'],
+        selector: 'typeAlias',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-import-type-side-effects': ['error'],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    curly: 'error',
+    'no-unused-vars': 'off',
+    'object-shorthand': ['error', 'always'],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/jsx-curly-brace-presence': 'error',
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+        ignoreCase: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+        shorthandLast: true,
+      },
+    ],
+    'react/no-unknown-property': ['error', { ignore: ['global', 'jsx'] }],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+        vars: 'all',
+        varsIgnorePattern: '^_',
+      },
+    ],
   },
-]
+}]
 
 export default eslintConfig
