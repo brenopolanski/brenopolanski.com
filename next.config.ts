b3f345776bea129260175405b5767fb4762791c3
brenopolanski <breno.polanski@gmail.com>
2026-08-15 10:17:48 -0300

    chore: update dependencies and remove unused configurations to streamline setup for Next.js project

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,5 @@
-import CircularDependencyPlugin from 'circular-dependency-plugin'
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  env: {
-    NEXT_TELEMETRY_DISABLED: '1',
-  },
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      // Add plugins in development mode
-      config.plugins.push(
-        new CircularDependencyPlugin({
-          allowAsyncCycles: false,
-          cwd: process.cwd(),
-          exclude: /node_modules/,
-          failOnError: true,
-          include: /src/,
-        }),
-      )
-    }
-
-    return config
-  },
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
